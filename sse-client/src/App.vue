2544f49c5320206c3ca8f4f681f9ae119a1e6130
yanghyeonjin <guswls7081@gmail.com>
2024-10-06 01:06:37 +0900

    cors 허용

--- sse-client/src/App.vue
+++ sse-client/src/App.vue
@@ -5,7 +5,13 @@
 </template>
 
 <script setup>
+import { onMounted } from 'vue';
 
+onMounted(async () => {
+  const response = await fetch("http://localhost:7777/posts");
+  const data = await response.json();
+  console.log(data);
+})
 </script>
 
 <style scoped>
